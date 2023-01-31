65ffe7d04e0fc3af0af95b5652c899f70a75c163
Lenovo <kannika.sb@rmuti.ac.th>
2023-01-31 17:09:35 +0700

    second commit 25/1/2023

--- ProfileApp/views.py
+++ ProfileApp/views.py
@@ -13,35 +13,6 @@ def interest(request):
     return render(request, 'profile/interest.html')
 def career(request):
     return render(request, 'profile/career.html')
-
-
-def lab10(request):
-    name = "กรรณิกา ศรีบุรินทร์์"
-    stdid = "6534230202-1"
-    address = "74 ม.3 ต.ท่าสะอาด อ.นาด้วง จ.เลย 42210"
-    gen = "หญิง"
-    weight = "50"
-    height = "163"
-    color = "สีดำ"
-    food = "ชาบู"
-    job = "นักศึกษา"
-    productlist = [
-                   ["JACQUEMUS Le bob Gadjo", 100, "images/s1.jpg"],
-                   ["PRADA Re-Nylon Bucket Hat", 150, "images/s2.jpg"],
-                   ["DIOR DIORESORT Small Brim Hat", 89, "images/pd3.jpg"],
-                   ["GUCCI GG Canvas Bucket Hat", 30, "images/s4.jpg"],
-                   ["CELINE Cotton Drill Baseball Cap", 49, "images/s5.jpg"],
-                   ["BURBERRY Vintage Check Technical Cotton Bucket Hat", 99, "images/s6.jpg"],
-                   ["PALM ANGELS Black Logo Cap", 49, "images/s7.jpg"],
-                   ["FENDI Fendirama Hat", 200, "images/s8.jpg"],
-                   ["LOEWE Bucket Hat in Anagram Jacquard and Calfskin", 69, "images/s9.jpg"],
-                   ["CHANEL Cloche Hat", 69, "images/s10.jpg"],
-                   ]
-    return render(request, 'Profile/lab10.html',{'name': name, 'stdid': stdid, 'address': address, 'gen': gen,
-                                              'weight': weight, 'height': height, 'color': color, 'food': food,
-                                              'job': job, 'productlist': productlist})
-
-
 def shop(request):
     details = "Gundam plastic model (Gunpla)"
     name = "น.ส. กรรณิกา ศรีบุรินทร์"
@@ -69,5 +40,33 @@ def shop(request):
     product.append(pd9)
     product.append(pd10)
     return render(request, 'shop.html', {'product': product,
-                                              'details': details, 'name': name,
-                                              'date': date.strftime("%A %d-%m-%Y %H : %M")})
+                                         'details': details, 'name': name,
+                                         'date': date.strftime("%A %d-%m-%Y %H : %M")})
+
+  
+
+def lab10(request):
+    name = "กรรณิกา ศรีบุรินทร์์"
+    stdid = "6534230202-1"
+    address = "74 ม.3 ต.ท่าสะอาด อ.นาด้วง จ.เลย 42210"
+    gen = "หญิง"
+    weight = "50"
+    height = "163"
+    color = "สีดำ"
+    food = "ชาบู"
+    job = "นักศึกษา"
+    productlist = [
+                   ["JACQUEMUS Le bob Gadjo", 100, "images/s1.jpg"],
+                   ["PRADA Re-Nylon Bucket Hat", 150, "images/s2.jpg"],
+                   ["DIOR DIORESORT Small Brim Hat", 89, "images/pd3.jpg"],
+                   ["GUCCI GG Canvas Bucket Hat", 30, "images/s4.jpg"],
+                   ["CELINE Cotton Drill Baseball Cap", 49, "images/s5.jpg"],
+                   ["BURBERRY Vintage Check Technical Cotton Bucket Hat", 99, "images/s6.jpg"],
+                   ["PALM ANGELS Black Logo Cap", 49, "images/s7.jpg"],
+                   ["FENDI Fendirama Hat", 200, "images/s8.jpg"],
+                   ["LOEWE Bucket Hat in Anagram Jacquard and Calfskin", 69, "images/s9.jpg"],
+                   ["CHANEL Cloche Hat", 69, "images/s10.jpg"],
+                   ]
+    return render(request, 'Profile/lab10.html',{'name': name, 'stdid': stdid, 'address': address, 'gen': gen,
+                                              'weight': weight, 'height': height, 'color': color, 'food': food,
+                                              'job': job, 'productlist': productlist})
