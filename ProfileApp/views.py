8890489abe97712f6dd3b150fd382ce67d9f3bd5
Lenovo <kannika.sb@rmuti.ac.th>
2023-01-31 17:06:08 +0700

    second commit 25/1/2023

--- ProfileApp/views.py
+++ ProfileApp/views.py
@@ -13,19 +13,7 @@ def interest(request):
     return render(request, 'profile/interest.html')
 def career(request):
     return render(request, 'profile/career.html')
-def shop(request):
-    myproduct = [  ["JACQUEMUS Le bob Gadjo", 100, "images/s1.jpg"],
-                   ["PRADA Re-Nylon Bucket Hat", 150, "images/s2.jpg"],
-                   ["DIOR DIORESORT Small Brim Hat", 89, "images/pd3.jpg"],
-                   ["GUCCI GG Canvas Bucket Hat", 30, "images/s4.jpg"],
-                   ["CELINE Cotton Drill Baseball Cap", 49, "images/s5.jpg"],
-                   ["BURBERRY Vintage Check Technical Cotton Bucket Hat", 99, "images/s6.jpg"],
-                   ["PALM ANGELS Black Logo Cap", 49, "images/s7.jpg"],
-                   ["FENDI Fendirama Hat", 200, "images/s8.jpg"],
-                   ["LOEWE Bucket Hat in Anagram Jacquard and Calfskin", 69, "images/s9.jpg"],
-                   ["CHANEL Cloche Hat", 69, "images/s10.jpg"],
-                   ]
-    return render(request, 'profile/shop.html', {'myproduct': myproduct})
+
 
 def lab10(request):
     name = "กรรณิกา ศรีบุรินทร์์"
@@ -54,7 +42,7 @@ def lab10(request):
                                               'job': job, 'productlist': productlist})
 
 
-def lab11(request):
+def shop(request):
     details = "Gundam plastic model (Gunpla)"
     name = "น.ส. กรรณิกา ศรีบุรินทร์"
     date = datetime.datetime.now()
@@ -80,6 +68,6 @@ def lab11(request):
     product.append(pd8)
     product.append(pd9)
     product.append(pd10)
-    return render(request, 'lab11.html', {'product': product,
+    return render(request, 'shop.html', {'product': product,
                                               'details': details, 'name': name,
                                               'date': date.strftime("%A %d-%m-%Y %H : %M")})
